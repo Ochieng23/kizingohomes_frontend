60dbe0e2c3554f411b8e1e9415359a2bab16878e
paul <ochiengpaul001@gmail.com>
2023-06-05 07:48:22 +0300

    create navbar skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from "./Components/Homepage"
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+  <Navbar/>
   <Routes>
-    <Route path='/' element={<Homepage/>} />
+    <Route path='/home' element={<Homepage/>} />
     <Route/>
     <Route/>
     <Route/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include JavaScript and JSX files
+    './public/index.html', // Include HTML files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
